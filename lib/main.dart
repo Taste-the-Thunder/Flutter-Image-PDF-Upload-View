ed9fa9c01e325f2d3d245611243cb24b1920fae9
dhruvinprajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2023-02-28 21:03:48 +0530

    image pick from gallery and camera both

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,12 +33,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  File? _image;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future getImage(ImageSource source) async{
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      final imageTemporary = File(image.path);
+
+      setState(() {
+        this._image = imageTemporary;
+      });
+    } on PlatformException catch (e){
+      print('Failed: $e');
+    }
+
   }
 
   @override
@@ -50,18 +63,32 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            _image != null ? Image.file(_image!, width: 250,height: 250, fit: BoxFit.cover,):Text('image'),
+            CustomButton(title:'pick from Gallery', icon:Icons.image_outlined, onClick:() => getImage(ImageSource.gallery)),
+            CustomButton(title:'pick from Camera', icon:Icons.camera, onClick: ()=>getImage(ImageSource.camera))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+}
+
+Widget CustomButton({
+  required String title,
+  required IconData icon,
+  required VoidCallback onClick,
+}){
+  return Container(
+    width: 280,
+    child: ElevatedButton(
+      onPressed: onClick,
+      child: Row(
+        children: [
+          Icon(icon),
+          SizedBox(width: 20,),
+          Text(title)
+        ],
+      ),
+    ),
+  );
 }
